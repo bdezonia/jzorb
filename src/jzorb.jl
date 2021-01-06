00fec98c131581a994f5ce2302da5f3e97634ef0
Barry DeZonia <bdezonia@gmail.com>
2021-01-06 01:04:37 -0600

    Make the code a little more sensible

--- src/jzorb.jl
+++ src/jzorb.jl
@@ -1,8 +1,10 @@
 
 function two(type)
+  a = construct(type)
+  b = construct(type)
   result = construct(type)
-  a = unity(type)
-  b = unity(type)
+  unity(a)
+  unity(b)
   add(a,b,result)
   return result
 end
@@ -15,8 +17,8 @@ function construct(::Type{SignedInt64Member})
   return SignedInt64Member(0)
 end
 
-function unity(::Type{SignedInt64Member})
-  return SignedInt64Member(1)
+function unity(a :: SignedInt64Member)
+  a.v = 1
 end
 
 function add(a :: SignedInt64Member, b :: SignedInt64Member, c :: SignedInt64Member)
@@ -31,8 +33,8 @@ function construct(::Type{Float64Member})
   return Float64Member(0.0)
 end
 
-function unity(::Type{Float64Member})
-  return Float64Member(1.0)
+function unity(a :: Float64Member)
+  a.v = 1.0
 end
 
 function add(a :: Float64Member, b :: Float64Member, c :: Float64Member)
@@ -50,8 +52,11 @@ function construct(::Type{Mat22})
    return Mat22(0.0,0.0,0.0,0.0)
 end
 
-function unity(::Type{Mat22})
-   return Mat22(1.0,0.0,0.0,1.0)
+function unity(a :: Mat22)
+  a.zz = 1.0
+  a.zo = 0.0
+  a.oz = 0.0
+  a.oo = 0.0
 end
 
 function add(a :: Mat22, b :: Mat22, c :: Mat22)
